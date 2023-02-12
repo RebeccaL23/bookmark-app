9a89ec0eac0b979fd2b6a16f49c369263a3c7774
Rebecca Lim <rebeccalim23@gmail.com>
2023-02-12 12:16:14 +0000

    load existing bookmark into form

--- bookmarker/src/App.tsx
+++ bookmarker/src/App.tsx
@@ -1,8 +1,7 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-import {nanoid} from "nanoid"
-import seed from "./seed"
+import {nanoid} from "nanoid" // for custom created ids
+import seed from "./seed" // "seed" file for default data
 
 
 function App() {
@@ -18,7 +17,7 @@ function App() {
     readonly key: string,
     readonly id: string,
     lastEdited: string,
-    notes?: string // optional field
+    notes?: string | null// optional field
   }
 
   // define bookmarks as an array of objects
@@ -38,14 +37,15 @@ function App() {
   const [currentBookmarkId, setCurrentBookmarkId] = useState(
     // initialises as the first bookmark's id or an empty string, if bookmark is not empty
     (bookmarks[0] && bookmarks[0].id) || ""
-  )
+    )
 
-  // every time the bookmarks array changes, then run the function to set local storage (key = bookmarks, value = bookmarks array stringified)
-  useEffect(() => {
-    localStorage.setItem("bookmarks", JSON.stringify(bookmarks))
-  }, [bookmarks])
+    // every time the bookmarks array changes, then run the function to set local storage (key = bookmarks, value = bookmarks array stringified)
+    useEffect(() => {
+      localStorage.setItem("bookmarks", JSON.stringify(bookmarks))
+    }, [bookmarks])
 
   ////////////////////////// FUNCTIONS //////////////////////////
+
   // get time / date now
   function lastEdited() {
     var currentdate = new Date();
@@ -75,12 +75,39 @@ function App() {
     setBookmarks((prevBookmarks: []) => [newBookmark, ...prevBookmarks])
   }
 
+  // load existing bookmark in form
+  function loadBookmark(id) {
+    // if bookmark id matches bookmark in array, then show the values of the bookmark in the form
+    const urlInput = document.getElementById("url-field")
+    const notesInput = document.getElementById("notes-field")
+
+    bookmarks.map(bookmark => {
+      if (bookmark.id === id) {
+        urlInput.value = bookmark.url
+        notesInput.value = bookmark.notes
+
+      }
+    })
+  }
+
+  // edit button
+  // const editButton = {
+  //   return (
+  //     <button className='btn' id='submit-button' style={submitDefault} onClick={() => editBookmark()} >Save</button>
+  //   )
+  // }
+
   // edit bookmark
-  function editBookmark(url: Url, notes: string) {
-    setBookmarks((prevBookmarks: []) => prevBookmarks.map((prevBookmark: {}) => {
-        return prevBookmark.id === currentBookmarkId
-            ? { ...prevBookmark, lastEdited: lastEdited(), url: url, notes: notes }
-            : prevBookmark
+  function editBookmark(event, id) {
+    // urlInput.value = bookmarks
+    const urlInput = document.getElementById("url-field")
+    const notesInput = document.getElementById("notes-field")
+
+    setBookmarks(prevBookmarks => prevBookmarks.map(bookmark => {
+        return bookmark.id === id ?
+          { ...bookmark, lastEdited: lastEdited(), url: urlInput, notes: notesInput }
+          :
+          bookmark
     }))
   }
 
@@ -131,14 +158,19 @@ function App() {
 
   return (
     <div className="App">
+
       <h1 style={displayHeading}>Add a bookmark</h1>
+
       <form id='bookmark-form' className={bookmarks.length === 0 ? 'form-default' : 'form-top'}>
         <input id='url-field' type="url" name="url" placeholder="Enter bookmark link" style={fieldDefault}></input>
         <input id='notes-field' type="text" name="notes" placeholder="Leave a note. 100 characters or less." style={fieldDefault}></input>
         <button className='btn' id='submit-button' style={submitDefault} onClick={() => createBookmark()} >Save</button>
+
         {/* <p>Error Message</p> */}
       </form>
+
       <table style={displayTable}>
+
         <thead>
           <tr>
             <th>Last edited</th>
@@ -147,23 +179,25 @@ function App() {
           </tr>
         </thead>
 
-          {bookmarks.map((bookmark: Bookmark) => (
-            <tbody>
-              <tr>
-                <td>{bookmark.lastEdited}</td>
-                <td><a target="_blank" href={bookmark.url}>{bookmark.url}</a></td>
-                <td>{bookmark.notes}</td>
-                <td id="edit" >Edit</td>
-                <td id="delete" onClick={(event) => deleteBookmark(event, bookmark.id)}>Delete</td>
-              </tr>
-            </tbody>
-          ))}
+        {bookmarks.map((bookmark: Bookmark) => (
+          <tbody>
+            <tr>
+              <td>{bookmark.lastEdited}</td>
+              <td><a target="_blank" href={bookmark.url}>{bookmark.url}</a></td>
+              <td>{bookmark.notes}</td>
+              <td id="edit" onClick={(event) => loadBookmark(bookmark.id)}>Edit</td>
+              <td id="delete" onClick={(event) => deleteBookmark(event, bookmark.id)}>Delete</td>
+            </tr>
+          </tbody>
+        ))}
 
       </table>
+
       <div id='footer'>
         <div id='pagination'>1, 2, 3</div>
         <p id='remove-all' style={display} onClick={(event) => deleteAll(event)}>Remove all bookmarks</p>
       </div>
+
     </div>
   )
 }
