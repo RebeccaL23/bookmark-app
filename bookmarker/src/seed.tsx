19956102e38f6213da0c9087235ddea5aa27f9f6
Rebecca Lim <rebeccalim23@gmail.com>
2023-02-14 11:58:22 +0000

    update seel file data

--- bookmarker/src/seed.tsx
+++ bookmarker/src/seed.tsx
@@ -5,162 +5,161 @@ export default [
     id: nanoid(),
     lastEditedRaw: new Date("2023-01-29").getTime(),
     lastEditedDate: "29/01/2023",
-    url: "https://phantom.land/work/festive-fiends"
+    notes: "What I'm currently listening to...",
+    url: "https://open.spotify.com/playlist/2zCrPFxZbStyuLyKb6s7C3?si=e9b12a8f9d184da6"
   },
   {
     id: nanoid(),
     lastEditedRaw: new Date("2023-01-12").getTime(),
     lastEditedDate: "12/01/2023",
+    notes: "Fave Phantom project!",
     url: "https://phantom.land/work/festive-fiends"
   },
   {
     id: nanoid(),
     lastEditedRaw: new Date("2023-01-05").getTime(),
     lastEditedDate: "05/01/2023",
-    url: "https://phantom.land/work/festive-fiends"
+    notes: "Realistic drawing of a girl made purely with CSS! Crazy.",
+    url: "https://codepen.io/ivorjetski/pen/dBYWWZ"
   },
   {
     id: nanoid(),
     lastEditedRaw: new Date("2023-01-05").getTime(),
     lastEditedDate: "05/01/2023",
-    url: "https://phantom.land/work/festive-fiends"
+    notes: "Ring inspo.",
+    url: "https://www.instagram.com/p/CYpLCdJth2j/?hl=en"
   },
   {
     id: nanoid(),
     lastEditedRaw: new Date("2023-01-04").getTime(),
     lastEditedDate: "04/01/2023",
-    url: "https://phantom.land/work/festive-fiends"
+    notes: "London-based agency to keep an 👀 on.",
+    url: "https://phantom.land/"
   },
   {
     id: nanoid(),
     lastEditedRaw: new Date("2023-01-04").getTime(),
     lastEditedDate: "04/01/2023",
-    url: "https://phantom.land/work/festive-fiends"
+    notes: "Coconut Miso Salmon Curry recipe–gotta try!",
+    url: "https://cooking.nytimes.com/recipes/1020045-coconut-miso-salmon-curry"
   },
   {
     id: nanoid(),
     lastEditedRaw: new Date("2023-01-04").getTime(),
     lastEditedDate: "04/01/2023",
-    url: "https://phantom.land/work/festive-fiends"
+    notes: "Melbourne-based digital agency with a class team! I might be biased...",
+    url: "https://www.bluerock.com.au/services/digital/"
   },
   {
     id: nanoid(),
     lastEditedRaw: new Date("2023-01-02").getTime(),
     lastEditedDate: "02/01/2023",
-    url: "https://phantom.land/work/festive-fiends"
+    notes: "Online boutique–clothes, accessories and shoes",
+    url: "https://sourceunknown.com/"
   },
   {
     id: nanoid(),
     lastEditedRaw: new Date("2023-01-02").getTime(),
     lastEditedDate: "02/01/2023",
-    url: "https://phantom.land/work/festive-fiends"
+    notes: "Love this platform–use it to keep track of all my personal stuff!",
+    url: "https://www.notion.so/"
   },
   {
     id: nanoid(),
     lastEditedRaw: new Date("2023-01-02").getTime(),
     lastEditedDate: "25/12/2022",
-    url: "https://phantom.land/work/festive-fiends"
+    notes: "Great design platform for non-designers",
+    url: "https://www.figma.com/"
   },
   {
     id: nanoid(),
     lastEditedRaw: new Date("2022-12-28").getTime(),
     lastEditedDate: "28/12/2022",
-    url: "https://phantom.land/work/festive-fiends"
+    notes: "Seriously BEST ever brownies",
+    url: "https://handletheheat.com/chewy-brownies/"
   },
   {
     id: nanoid(),
     lastEditedRaw: new Date("2022-12-25").getTime(),
     lastEditedDate: "25/12/2022",
-    url: "https://phantom.land/work/festive-fiends"
+    notes: "Ring inspo! Love this.",
+    url: "hhttps://hernanherdez.com/collections/all/products/domo"
   },
   {
     id: nanoid(),
     lastEditedRaw: new Date("2022-12-24").getTime(),
     lastEditedDate: "24/12/2022",
-    url: "https://phantom.land/work/festive-fiends"
+    notes: "Dev online courses",
+    url: "https://scrimba.com/allcourses"
   },
   {
     id: nanoid(),
     lastEditedRaw: new Date("2022-12-24").getTime(),
     lastEditedDate: "24/12/2022",
-    notes: "Love this brand! On the lookout for new swimmers.",
-    url: "https://phantom.land/work/festive-fiends"
+    notes: "Check out this Malaysian independent bag label, founded and run by one family",
+    url: "https://www.instagram.com/repleatco/"
   },
   {
     id: nanoid(),
     lastEditedRaw: new Date("2022-12-22").getTime(),
     lastEditedDate: "22/12/2022",
-    notes: "Love this brand! On the lookout for new swimmers.",
-    url: "https://phantom.land/work/festive-fiends"
+    notes: "Vintage fair in London",
+    url: "https://clerkenwellvintagefashionfair.co.uk/"
   },
   {
     id: nanoid(),
     lastEditedRaw: new Date("2022-12-22").getTime(),
     lastEditedDate: "21/12/2022",
-    notes: "Love this brand! On the lookout for new swimmers.",
-    url: "https://phantom.land/work/festive-fiends"
+    notes: "Next on the reading list!",
+    url: "https://www.ynharari.com/book/sapiens-2/"
   },
   {
     id: nanoid(),
     lastEditedRaw: new Date("2022-12-20").getTime(),
     lastEditedDate: "20/12/2022",
-    notes: "Love this brand! On the lookout for new swimmers.",
-    url: "https://phantom.land/work/festive-fiends"
+    notes: "Today Explained by Vox podcast",
+    url: "https://open.spotify.com/show/3pXx5SXzXwJxnf4A5pWN2A?si=101fd48ef30b486b"
   },
   {
     id: nanoid(),
     lastEditedRaw: new Date("2022-12-19").getTime(),
     lastEditedDate: "19/12/2022",
-    notes: "Love this brand! On the lookout for new swimmers.",
-    url: "https://phantom.land/work/festive-fiends"
+    notes: "Cute frame",
+    url: "https://www.instagram.com/p/CjI0MB4L09K/?utm_source=ig_web_copy_link"
   },
   {
     id: nanoid(),
     lastEditedRaw: new Date("2022-12-19").getTime(),
     lastEditedDate: "19/12/2022",
-    notes: "Love this brand! On the lookout for new swimmers.",
-    url: "https://phantom.land/work/festive-fiends"
+    notes: "99% Invisible podcast",
+    url: "https://open.spotify.com/show/2VRS1IJCTn2Nlkg33ZVfkM?si=0e105f307fa54da1"
   },
   {
     id: nanoid(),
     lastEditedRaw: new Date("2022-12-19").getTime(),
     lastEditedDate: "19/12/2022",
-    notes: "Love this brand! On the lookout for new swimmers.",
-    url: "https://phantom.land/work/festive-fiends"
+    notes: "Super cool developer portfolio site.",
+    url: "https://www.gardener.nyc/"
   },
   {
     id: nanoid(),
     lastEditedRaw: new Date("2022-12-12").getTime(),
     lastEditedDate: "12/12/2022",
-    notes: "Love this brand! On the lookout for new swimmers.",
-    url: "https://phantom.land/work/festive-fiends"
+    notes: "Youtube channel celebrating musicians from all over the world",
+    url: "https://www.youtube.com/@COLORSxSTUDIOS"
   },
   {
     id: nanoid(),
     lastEditedRaw: new Date("2022-12-11").getTime(),
     lastEditedDate: "11/12/2022",
-    notes: "Next on the booklist",
-    url: "https://phantom.land/work/festive-fiends"
+    notes: "Recipe page for great salad-esque, veggie-filled meals",
+    url: "https://www.tiktok.com/@justine_snacks?lang=en"
   },
   {
     id: nanoid(),
     lastEditedRaw: new Date("2022-12-11").getTime(),
     lastEditedDate: "11/12/2022",
-    notes: "Inspo for jewellery-making project",
-    url: "https://phantom.land/work/festive-fiends"
-  },
-  {
-    id: nanoid(),
-    lastEditedRaw: new Date("2022-12-08").getTime(),
-    lastEditedDate: "08/12/2022",
-    notes: "London-based agency to keep an 👀 on.",
-    url: "https://phantom.land/work/festive-fiends"
-  },
-  {
-    id: nanoid(),
-    lastEditedRaw: new Date("2022-10-18").getTime(),
-    lastEditedDate: "18/10/2022",
-    notes: "Love this brand! On the lookout for new swimmers.",
-    url: "https://phantom.land/work/festive-fiends"
-  },
+    notes: "Fave song at the moment.",
+    url: "https://open.spotify.com/track/1knUACOt5yfNAYn7gnYQVt?si=2eca8bf63c1d4654"
+  }
 ]
